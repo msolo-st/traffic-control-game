2ff81744475dbd51b0be2e4c743b4e2107ceb030
ItsMatty <msolo5@illinois.edu>
2025-07-20 09:14:15 -0500

    Created Main Game Loop

--- service/main.py
+++ service/main.py
@@ -2,7 +2,18 @@ import pygame
 print(pygame.__version__)
 pygame.init()
 
-
+#Main Game Loop
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Traffic Control")
 clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(60) #60 FPS
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #Update game state
+    #Draw everything
+    pygame.display.flip()
+    pygame.quit()

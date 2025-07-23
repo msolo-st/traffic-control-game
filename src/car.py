4600482658771f41cd63da63f759f9e82f276f90
ItsMatty <msolo5@illinois.edu>
2025-07-22 19:42:19 -0500

    Reformmated Project Folder Structure

---
+++ src/car.py
@@ -0,0 +1,17 @@
+import pygame
+import random
+
+class Car:
+    def __init__(self, image, x, y, speed):
+        self.image = pygame.image.load(image).convert_alpha()
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.speed = speed
+
+    def update(self):
+        self.rect.y += self.speed
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def off_screen(self, height):
+        return self.rect.top > height
